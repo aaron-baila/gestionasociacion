b32919f36d5fad11c1c3922fca4ff29512274d45
Aaron <aaron.e.baila@gmail.com>
2024-12-27 18:14:49 +0100

    Initial commit

---
+++ gestionasociacion/src/main/java/com/monterdealbarracin/gestionasociacion/GestionasociacionApplication.java
@@ -0,0 +1,13 @@
+package com.monterdealbarracin.gestionasociacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionasociacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionasociacionApplication.class, args);
+	}
+
+}
---
+++ gestionasociacion/src/test/java/com/monterdealbarracin/gestionasociacion/GestionasociacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monterdealbarracin.gestionasociacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionasociacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
