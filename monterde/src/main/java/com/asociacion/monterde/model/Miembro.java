8e42bc0ac9ccd470896e45226e6d1edcafc48dac
Aaron <aaron.e.baila@gmail.com>
2025-01-16 13:34:40 +0100

    miembro model

---
+++ monterde/src/main/java/com/asociacion/monterde/model/Miembro.java
@@ -0,0 +1,43 @@
+package com.asociacion.monterde.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Miembro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-incremento para el ID
+    private Long id;
+
+    private String nombre;   // Nombre del miembro
+    private String email;    // Correo electrónico
+    private String telefono; // Teléfono de contacto
+    private String rol;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Miembro miembro = (Miembro) o;
+        return getId() != null && Objects.equals(getId(), miembro.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
