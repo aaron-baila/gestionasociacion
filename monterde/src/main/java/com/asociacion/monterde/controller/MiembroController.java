58baa4a3b709614e43e1fcaa9caa247abba7f863
Aaron <aaron.e.baila@gmail.com>
2025-01-16 23:13:58 +0100

    eliminar ok

--- monterde/src/main/java/com/asociacion/monterde/controller/MiembroController.java
+++ monterde/src/main/java/com/asociacion/monterde/controller/MiembroController.java
@@ -9,20 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/miembros")
 public class MiembroController {
 
     @Autowired
     private MiembroService miembroService;
 
-    @GetMapping("/miembros")
+    @GetMapping
     public String listarMiembros(Model model) {
         model.addAttribute("miembros", miembroService.obtenerTodosLosMiembros());
         return "miembros";
     }
 
-    @DeleteMapping("/eliminar/{id}")
+//    @DeleteMapping("/eliminar/{id}")
+    @GetMapping("/eliminar/{id}")
     public String eliminarMiembro(@PathVariable Long id) {
         miembroService.eliminarMiembro(id); // Llama al servicio
         return "redirect:/miembros"; // Redirige tras eliminar
